b405d63d665b4760938188a223ed1fcff45910b0
Dennis Glasberg <dennis.glasberg@flywire.com>
2021-05-24 13:20:59 -0400

    Added Disneyland Parks

--- node_helper.js
+++ node_helper.js
@@ -6,103 +6,125 @@ const DisneyWorldMagicKingdom = new Themeparks.Parks.WaltDisneyWorldMagicKingdom
 const DisneyWorldEpcot = new Themeparks.Parks.WaltDisneyWorldEpcot();
 const DisneyWorldHollywoodStudios = new Themeparks.Parks.WaltDisneyWorldHollywoodStudios();
 const DisneyWorldAnimalKingdom = new Themeparks.Parks.WaltDisneyWorldAnimalKingdom();
+const DisneyLandMagicKingdom = new Themeparks.Parks.DisneylandResortMagicKingdom();
+const DisneyLandCaliforniaAdventure = new Themeparks.Parks.DisneylandResortCaliforniaAdventure();
 const DisneyWorldParisMagicKingdom = new Themeparks.Parks.DisneylandParisMagicKingdom();
 const DisneyWorldParisWaltDisneyStudios = new Themeparks.Parks.DisneylandParisWaltDisneyStudios();
 
 module.exports = NodeHelper.create({
-	start: function() {
-		console.log("Starting module helper: " + this.name);
-	},
+  start: function () {
+    console.log("Starting module helper: " + this.name);
+  },
 
-	processWaitTimes: function(park) {
-		var self = this;
-		console.log(park.name + ": Processing Wait Times...");
+  processWaitTimes: function (park) {
+    var self = this;
+    console.log(park.name + ": Processing Wait Times...");
 
-		const CheckOpeningTimes = (disneyPark) => {
-			disneyPark.GetOpeningTimes().then((times) => {
-				var openingTime;
-				var closingTime;
-				for(var i=0, time; time=times[i++];) {
-					if (time.type == "Operating") {
-						if (new Date() >= new Date(time.openingTime) && new Date() <= new Date(time.closingTime)) {
-							openingTime = new Date(time.openingTime);
-							closingTime = new Date(time.closingTime);
-						}
-						else if (new Date(time.openingTime).getDate() == new Date().getDate()) {
-							openingTime = new Date(time.openingTime);
-							closingTime = new Date(time.closingTime);
-						}
-					}
-				}
+    const CheckOpeningTimes = (disneyPark) => {
+      disneyPark
+        .GetOpeningTimes()
+        .then((times) => {
+          var openingTime;
+          var closingTime;
+          for (var i = 0, time; (time = times[i++]); ) {
+            if (time.type == "Operating") {
+              if (
+                new Date() >= new Date(time.openingTime) &&
+                new Date() <= new Date(time.closingTime)
+              ) {
+                openingTime = new Date(time.openingTime);
+                closingTime = new Date(time.closingTime);
+              } else if (
+                new Date(time.openingTime).getDate() == new Date().getDate()
+              ) {
+                openingTime = new Date(time.openingTime);
+                closingTime = new Date(time.closingTime);
+              }
+            }
+          }
 
-				var payload = {
-					openingTime: openingTime || null,
-					closingTime: closingTime || null,
-				};
+          var payload = {
+            openingTime: openingTime || null,
+            closingTime: closingTime || null
+          };
 
-				self.sendSocketNotification("POPULATE_OPENING_TIMES_" + park.name.replace(/ /g,"_"), payload);
-			}).catch((error) => {
-				console.log(park.name + ": Error Processing Opening Times...");
-				console.error(error);
-			});
-		};
+          self.sendSocketNotification(
+            "POPULATE_OPENING_TIMES_" + park.name.replace(/ /g, "_"),
+            payload
+          );
+        })
+        .catch((error) => {
+          console.log(park.name + ": Error Processing Opening Times...");
+          console.error(error);
+        });
+    };
 
-		const CheckWaitTimes = (disneyPark) => {
-			disneyPark.GetWaitTimes().then((rides) => {
-				waitTimes = [];
-				for(var i = 0, ride; ride = rides[i++];) {
-					if (park.logRide) {
-						console.log(ride.name);
-						console.log(park.rides);
-					}
-					if (park.rides && park.rides.includes(ride.name)) {
-						waitTimes.push(ride);
-					}
-				}
-				console.log(park.name + ": Processed Wait Times...");
+    const CheckWaitTimes = (disneyPark) => {
+      disneyPark
+        .GetWaitTimes()
+        .then((rides) => {
+          waitTimes = [];
+          for (var i = 0, ride; (ride = rides[i++]); ) {
+            if (park.logRide) {
+              console.log(ride.name);
+              console.log(park.rides);
+            }
+            if (park.rides && park.rides.includes(ride.name)) {
+              waitTimes.push(ride);
+            }
+          }
+          console.log(park.name + ": Processed Wait Times...");
 
-				var payload = {
-					waitTimes: waitTimes
-				};
+          var payload = {
+            waitTimes: waitTimes
+          };
 
-				self.sendSocketNotification("POPULATE_WAIT_TIMES_" + park.name.replace(/ /g,"_"), payload);
-			}).catch((error) => {
-				console.log(park.name + ": Error Processing Wait Times...");
-				console.error(error);
-			});
-		};
+          self.sendSocketNotification(
+            "POPULATE_WAIT_TIMES_" + park.name.replace(/ /g, "_"),
+            payload
+          );
+        })
+        .catch((error) => {
+          console.log(park.name + ": Error Processing Wait Times...");
+          console.error(error);
+        });
+    };
 
-		switch (park.name) {
-		case "Magic Kingdom":
-			CheckOpeningTimes(DisneyWorldMagicKingdom);
-			CheckWaitTimes(DisneyWorldMagicKingdom);
-			break;
-		case "Epcot":
-			CheckOpeningTimes(DisneyWorldEpcot);
-			CheckWaitTimes(DisneyWorldEpcot);
-			break;
-		case "Hollywood Studios":
-			CheckOpeningTimes(DisneyWorldHollywoodStudios);
-			CheckWaitTimes(DisneyWorldHollywoodStudios);
-			break;
-		case "Animal Kingdom":
-			CheckOpeningTimes(DisneyWorldAnimalKingdom);
-			CheckWaitTimes(DisneyWorldAnimalKingdom);
-			break;
-		case "Magic Kingdom - Disneyland Paris":
-			CheckOpeningTimes(DisneyWorldParisMagicKingdom);
-			CheckWaitTimes(DisneyWorldParisMagicKingdom);
-			break;
-		case "Walt Disney Studios - Disneyland Paris":
-			CheckOpeningTimes(DisneyWorldParisWaltDisneyStudios);
-			CheckWaitTimes(DisneyWorldParisWaltDisneyStudios);
-			break;
-		}
-	},
+    switch (park.name) {
+      case "Magic Kingdom":
+        CheckOpeningTimes(DisneyWorldMagicKingdom);
+        CheckWaitTimes(DisneyWorldMagicKingdom);
+        break;
+      case "Epcot":
+        CheckOpeningTimes(DisneyWorldEpcot);
+        CheckWaitTimes(DisneyWorldEpcot);
+        break;
+      case "Hollywood Studios":
+        CheckOpeningTimes(DisneyWorldHollywoodStudios);
+        CheckWaitTimes(DisneyWorldHollywoodStudios);
+        break;
+      case "Animal Kingdom":
+        CheckOpeningTimes(DisneyWorldAnimalKingdom);
+        CheckWaitTimes(DisneyWorldAnimalKingdom);
+        break;
+      case "Magic Kingdom - Disneyland Paris":
+        CheckOpeningTimes(DisneyWorldAnimalKingdom);
+        CheckWaitTimes(DisneyWorldAnimalKingdom);
+        break;
+      case "Magic Kingdom - Disneyland Resort":
+        CheckOpeningTimes(DisneyLandMagicKingdom);
+        CheckWaitTimes(DisneyLandMagicKingdom);
+        break;
+      case "California Adventure - Disneyland Resort":
+        CheckOpeningTimes(DisneyLandCaliforniaAdventure);
+        CheckWaitTimes(DisneyLandCaliforniaAdventure);
+        break;
+    }
+  },
 
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === "GET_WAIT_TIMES") {
-			this.processWaitTimes(payload);
-		}
-	},
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "GET_WAIT_TIMES") {
+      this.processWaitTimes(payload);
+    }
+  }
 });
